279cf916d6cb27f7dfca4ff8e7b0c1fc3a088a0b
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-09-24 17:20:03 -0700

    fix output

--- create-ec2-basic.sh
+++ create-ec2-basic.sh
@@ -2,38 +2,41 @@
 
 # Basic EC2 automation script 
 
+# Please note: 
+# Uses default region
+# Requires no input 
+# Edit line 11-13
+
 # Default values
-SG_NAME="default-sg"
-KEY_NAME="default-key"
+SG_NAME="default-sg-test"
+KEY_NAME="default-key-test"
+INSTANCE_NAME="my-ec2-instance"
 
 # Get the first Subnet ID
-SUBNET_ID=$(aws ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text)
-echo "Using Subnet ID: $SUBNET_ID"
+SUBNET_ID=$(aws ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text) > /dev/null
 
-# Create Security Group
-SG_ID=$(aws ec2 create-security-group --group-name $SG_NAME --description "My security group" --query 'GroupId' --output text)
-echo "Created Security Group with ID: $SG_ID"
-
-# Add SSH Inbound Rule to Security Group
-aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 22 --cidr 0.0.0.0/0
-echo "Added SSH inbound rule to Security Group"
+# Configure Security Group
+aws ec2 create-security-group --group-name $SG_NAME --description "My security group" > /dev/null
+SG_ID=$(aws ec2 describe-security-groups --group-names $SG_NAME --query 'SecurityGroups[0].GroupId' --output text) > /dev/null
+aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 22 --cidr 0.0.0.0/0 > /dev/null
 
 # Create Key Pair
-aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $KEY_NAME.pem
-chmod 400 $KEY_NAME.pem
-echo "Created Key Pair and saved to $KEY_NAME.pem"
+aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $KEY_NAME.pem > /dev/null
+chmod 400 $KEY_NAME.pem > /dev/null
 
 # Get Amazon Linux 2023 AMI ID
-AMI_ID=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2" --query "Images[0].ImageId" --output text)
-echo "Amazon Linux 2023 AMI ID: $AMI_ID"
+AMI_ID=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2" --query "Images[0].ImageId" --output text) > /dev/null
+
+# EC2 Instance Creation/Connection ----------
 
 # Create EC2 Instance
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID --query 'Instances[0].InstanceId' --output text)
-echo "Created EC2 Instance with ID: $INSTANCE_ID"
+aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --key-name $KEY_NAME --security-group-ids $SG_ID --subnet-id $SUBNET_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" > /dev/null
+
+# Get Instance ID
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" --query "Reservations[*].Instances[*].InstanceId" --output text) > /dev/null
 
 # Get Public DNS of the EC2 Instance
-PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PublicDnsName" --output text)
-echo "EC2 Instance Public DNS: $PUBLIC_DNS"
+PUBLIC_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].PublicDnsName" --output text) > /dev/null
 
 # Instructions to SSH into the EC2 Instance
 echo "To SSH into your EC2 instance, use the following command:"
